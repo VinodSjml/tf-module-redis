be6e1e454d35c596568e9e2b9c2f7503c53f93cd
vinod sjml <vinsjml@gmail.com>
2024-02-14 20:09:52 +0530

    push

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "redis_dns" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_elasticache_cluster.redis.cache_nodes.0.address]
+}
